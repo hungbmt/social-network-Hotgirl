f98f86e8fe72f5537df928563b0fc633d9bbf2c6
hungbmt <talaprox1@gmail.com>
2023-08-16 22:37:40 +0700

    add router

--- frontend/src/Router/index.js
+++ frontend/src/Router/index.js
@@ -3,6 +3,7 @@ import Home from "../Page/Home/Home";
 
 const publicRouter = [
   { path: "/", component: Home },
+  { path: "/login", component: Home },
   //   { path: "/:slug2/:slug/:sttnb", component: SubPage, Layout: SubPageLayOut },
 ];
 const priveteRouter = [];
